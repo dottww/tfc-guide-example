3dc439c726ad610943f1ec3380b8f7afcd813b15
dottww <wei-chen.lin@hashicorp.com>
2021-11-15 13:06:33 +0100

    medium

--- variables.tf
+++ variables.tf
@@ -5,11 +5,11 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Terraform-text-new12after-vcs-sentinel-test3"
+  default     = "Provisioned by Terraform-text-new12after-vcs-sentinel-test4"
 }
 
